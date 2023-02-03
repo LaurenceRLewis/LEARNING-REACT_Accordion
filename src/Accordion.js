a32dd0c15623ab85cd18044ea2a53c03d5128da7
Laurence Lewis (he/him) <laurence@a11ycats.com.au>
2023-02-03 14:23:06 +1000

    initial commit

--- src/Accordion.js
+++ src/Accordion.js
@@ -11,25 +11,24 @@ const Accordion = ({ items }) => {
   return (
     <div className="accordion">
       {items.map((item, index) => (
-        <div
-          className="accordion-item"
-          key={item.title + index}
-          role="presentation"
-        >
-          <div
+        <div className="accordion-item" key={item.title + index}>
+          <button
+            type="button"
+            aria-expanded={activeIndex === index ? "true" : "false"}
+            aria-controls={`accordion-body-${index}`}
+            id={`btn-${index}`}
             className="accordion-header"
-            id={`heading-${index}`}
-            role="tab"
             onClick={() => onTitleClick(index)}
           >
-            <h2 className="accordion-title">{item.title}</h2>
-          </div>
+            {item.title}
+          </button>
           <div
+            role="tabpanel"
             className={`accordion-body ${
               activeIndex === index ? "show" : "hide"
             }`}
-            aria-labelledby={`heading-${index}`}
-            role="tabpanel"
+            aria-labelledby={`btn-${index}`}
+            id={`accordion-body-${index}`}
           >
             {item.content}
           </div>
